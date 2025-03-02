533d075e65fd3c5289087806c5a1a3007ac236e5
Gabriel Ferreira de França <gabriel.ferreira.franca.84@gmail.com>
2025-03-02 16:29:28 -0300

    update

--- scripts/checkbox.js
+++ scripts/checkbox.js
@@ -1,12 +1,3 @@
-const toggleCheckbox = document.getElementById('toggle');
-
-// Adiciona o evento de mudança no checkbox
-toggleCheckbox.addEventListener('change', () => {
-    if (toggleCheckbox.checked) {
-        // Adiciona a classe para o modo claro
-        document.body.classList.add('light-mode');
-    } else {
-        // Remove a classe para voltar ao modo padrão
-        document.body.classList.remove('light-mode');
-    }
-});
+document.getElementById('toggle').addEventListener('change', (e) => 
+    document.body.classList.toggle('light-mode', e.target.checked)
+);
